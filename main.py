47a2fc67827fd9f1ed5e13669eae86c3d005fd3d
charittemi <risamyst@gmail.com>
2023-09-17 08:23:43 +0900

    modified:   main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ st.text('')
 
 # iframeを埋め込む
 iframe_html = """
-<iframe src="http://localhost:8501/path/to/test.md" width="100%" height="1000"></iframe>
+<iframe src="http://localhost:8501/path/to/test.md" width="100%"></iframe>
 """
 
 st.components.v1.html(iframe_html)
