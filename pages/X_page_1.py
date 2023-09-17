df619cb88df7a7d9fb4d1b9d1c5bc6968cf5ef3b
charittemi <risamyst@gmail.com>
2023-09-17 16:15:42 +0900

    changed

--- pages/X_page_1.py
+++ pages/X_page_1.py
@@ -15,10 +15,10 @@ st.text('')
 
 col1,col2=st.columns([1,2]) # カラムの比率1:2で設定
 with col1:
-    st.text('カラム１')
+    st.text('')
     
 with col2:
-    st.text('カラム２')
+    st.text('')
 
 markdown = """
 
@@ -35,22 +35,3 @@ $\displaystyle\lim_{a\to 0}f(x + a)$
 """
 st.markdown(markdown)
 
-    #コード表示用
-code='''
-markdown = """
-
-### 数式表示 
-
-$f(x) = x^2 + 1$
-
-$(1 + 2) \times 3 - 4 \div 2 = 7$
-
-$x^2$
-
-$\displaystyle\lim_{a\to 0}f(x + a)$
-
-
-"""
-st.markdown(markdown)
-'''
-st.code()
