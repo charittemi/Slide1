93f3ad93942a49ff3355ace01742dc930bd73b62
charittemi <risamyst@gmail.com>
2023-09-17 09:03:15 +0900

    new

---
+++ main.py
@@ -0,0 +1,17 @@
+﻿# streamlit >streamlit run main.py
+
+import streamlit as st
+from PIL import Image
+
+st.title('タイトル')
+st.caption('これはテスト用に作ったページです')
+st.subheader('About')
+st.text('streamlitでテストページを作ってみました')
+
+image=Image.open('./img/amago.jpg')
+st.image(image, width=250)
+st.text('')
+
+
+st.text('ok')
+
