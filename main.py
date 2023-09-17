62bf7d6118b72df241147fade7377eb234f79c1b
charittemi <risamyst@gmail.com>
2023-09-17 09:21:16 +0900

    new

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 ﻿# streamlit >streamlit run main.py
 
 import streamlit as st
-from PIL import Image
 
 st.title('タイトル')
-st.caption('これはテスト用に作ったページです')
+st.caption('これはスライド表示確認用に作ったページです')
 st.subheader('About')
-st.text('streamlitでテストページを作ってみました')
+st.text('streamlitを使って作成し、スライドのhtmlを埋め込んでいます')
 
-image=Image.open('./imgs/amago.jpg')
-st.image(image, width=250)
 st.text('')
 
 # Slide1.htmlを埋め込む
@@ -19,3 +16,10 @@ with open('Slide1.html', 'r', encoding='utf-8') as html_file:
 st.components.v1.html(slide_html, width=800, height=600)
 st.text('ok')
 
+'''
+# Slide1.htmlを埋め込む
+with open('Slide1.html', 'r', encoding='utf-8') as html_file:
+    slide_html = html_file.read()
+
+st.components.v1.html(slide_html, width=800, height=600)
+'''
