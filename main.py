6fa5b296bc548c470d65407e9d029bd76d1710af
charittemi <risamyst@gmail.com>
2023-09-17 15:41:18 +0900

    test

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import streamlit as st
 
 st.title('Main')
-st.subheader('スライド置き場')
-st.text('Marpで複数カラムのレイアウトにするには  \nカスタムCSSを別途用意する必要があります。')
+st.subheader('スライド作成メモ')
+st.text('Marpで作ったスライドを複数カラムのレイアウトにするには  \nカスタムCSSを別途用意する必要があります。')
 
 st.text('')
