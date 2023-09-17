d5d8e7232b98bd8dd1d84edb24d2acd29502797b
charittemi <risamyst@gmail.com>
2023-09-18 08:04:43 +0900

    c

--- pages/X_page_1.py
+++ pages/X_page_1.py
@@ -8,16 +8,15 @@ from PIL import Image
 
 st.title('Page 1')
 st.caption('')
-st.subheader('')
 
 st.text('')
 st.text('')
-
+st.subheader('Markdownで数式を書く')
 col1,col2=st.columns([1,2]) # カラムの比率1:2で設定
 with col1:
     markdown = r"""
 
-    #### Markdownで数式
+    ##### 表示結果
 
     $f(x) = x^2 + 1$
 
@@ -45,7 +44,7 @@ with col2:
 
     $\displaystyle\lim_{a \to 0}f(x + a)$
     ```
-    <!-- 文字列の先頭にrをつける(```markdown = r```)ことで、pythonのraw stringとして扱われる-->
+    <!-- ヒント：文字列の先頭にrをつける(markdown = r)ことで、pythonのraw stringとして扱われる-->
     
     """
     st.markdown(markdown)
