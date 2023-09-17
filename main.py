ba6cdeade3dc5b314b85a06b88a12d7277a5f137
charittemi <risamyst@gmail.com>
2023-09-17 09:13:07 +0900

    modified

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-﻿# streamlit >streamlit run main.py
-
-import streamlit as st
-from PIL import Image
+﻿import streamlit as st
+import streamlit.components.v1 as components
 
 st.title('タイトル')
 st.caption('これはテスト用に作ったページです')
 st.subheader('About')
 st.text('streamlitでテストページを作ってみました')
 
-image=Image.open('./imgs/amago.jpg')
+image = Image.open('./imgs/amago.jpg')
 st.image(image, width=250)
 st.text('')
-
-
 st.text('ok')
 
+# test.mdファイルをブロックで埋め込んで表示
+with open('test.md', 'r', encoding='utf-8') as markdown_file:
+    markdown_text = markdown_file.read()
+components.html(markdown_text, width=800, height=400)
