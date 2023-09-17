d58364471ed93d1dc8ce13e2f856f01e5255ad1c
charittemi <risamyst@gmail.com>
2023-09-17 14:02:59 +0900

    changed

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 
 import streamlit as st
 
-st.title('Slide(Marpで複数カラムレイアウトのスライド)')
-st.caption('mdファイルから作成したhtmlを埋め込んでいます')
-st.text('Marpで複数カラムのレイアウトにするにはカスタムCSSを別途用意する必要あり')
+st.title('Slide')
+st.caption('')
+st.subheader('Marpで複数カラムレイアウトのスライド作成')
+st.text('mdファイルから作成したhtmlを埋め込んでいます. Marpで複数カラムのレイアウトにするにはカスタムCSSを別途用意する必要あり')
 
 # Slide1.htmlを埋め込む
 with open('Slide1.html', 'r', encoding='utf-8') as html_file:
@@ -12,7 +13,7 @@ with open('Slide1.html', 'r', encoding='utf-8') as html_file:
 
 st.components.v1.html(slide_html, width=720, height=540)
 st.text('')
-st.subheader('コード')
+
 code='''
 # Slide1.htmlを埋め込む
 with open('Slide1.html', 'r', encoding='utf-8') as html_file:
