763787b9bd94ed0599638255ace7d9ce1b32b7a1
charittemi <risamyst@gmail.com>
2023-09-17 16:10:53 +0900

    changed

--- pages/X_page_1.py
+++ pages/X_page_1.py
@@ -20,11 +20,30 @@ with col1:
 with col2:
     st.text('カラム２')
 
+markdown = """
+# タイトル
+## ヘッダ
 
-    
-    
+### 数式インライン表示 
+
+$f(x) = x^2 + 1$
+
+$(1 + 2) \times 3 - 4 \div 2 = 7$
+
+$\displaystyle\frac{x}{y}$
+
+$x^2$
+
+$\displaystyle\lim_{a\to 0}f(x + a)$
+<br>
+
+### ブロック表示
+$$f(x) = x^2 + 1$$
+
+"""
+st.markdown(markdown)
+
     #コード表示用
 code='''
-
 '''
 st.code(code,language='python')
