82c109dfbad8e9e017808cac72691d4a908689c0
charittemi <risamyst@gmail.com>
2023-09-17 10:40:16 +0900

    changes

--- main.py
+++ main.py
@@ -2,11 +2,8 @@
 
 import streamlit as st
 
-st.title('タイトル')
-st.caption('これはスライド表示確認用に作ったページです')
-st.subheader('About')
-st.text('streamlitを使って作成し、スライドのhtmlを埋め込んでいます')
-
+st.title('Slide')
+st.caption('mdファイルから作成したhtmlを埋め込んでいます')
 st.text('')
 
 # Slide1.htmlを埋め込む
@@ -14,8 +11,8 @@ with open('Slide1.html', 'r', encoding='utf-8') as html_file:
     slide_html = html_file.read()
 
 st.components.v1.html(slide_html, width=800, height=600)
-st.text('ok')
-
+st.text('')
+st.subheader('コード')
 code='''
 # Slide1.htmlを埋め込む
 with open('Slide1.html', 'r', encoding='utf-8') as html_file:
