fde43c52a7867880bb7f3edf981d1a2728e3ca87
charittemi <risamyst@gmail.com>
2023-09-30 23:26:21 +0900

    new

---
+++ mdfiles/note.md
@@ -0,0 +1,5 @@
+# メモ
+## 今後の課題・興味
+- Docker
+- OpenInterpreterなど
+- 
---
+++ pages/notes.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Markdownファイルのパスを指定
+md_file_path = '\mdfiles\note.md'
+
+# ファイル読み込み
+with open(md_file_path, 'r') as file:
+    markdown_text = file.read()
+
+# StreamlitでMarkdownを表示します
+st.markdown(markdown_text)
